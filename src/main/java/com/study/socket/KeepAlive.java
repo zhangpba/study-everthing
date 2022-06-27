0574ca2c329eded3f2921dacee1bb34042df64b8
answerzhang <answerzhang@tencent.com>
2022-06-27 21:00:19 +0800

    socket

---
+++ src/main/java/com/study/socket/KeepAlive.java
@@ -0,0 +1,23 @@
+package com.study.socket;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author zhangpba
+ * @description 维持连接的消息对象（心跳对象）
+ * @date 2022/5/27
+ */
+public class KeepAlive implements Serializable {
+
+    private static final long serialVersionUID = -2813120366138988480L;
+
+    /* 覆盖该方法，仅用于测试使用。
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "\t维持连接包";
+    }
+}
